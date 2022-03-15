756633f472be81e7125d7373b303a5bc96f1bfad
Zash <zeeshan2211997@gmail.com>
2022-03-15 14:58:00 +0530

    Navigation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// ===Typing Animation====
+// ===Typing Animation==== //
 
 var typed = new Typed(".typing",{
     strings:["","Frontend Developer","Web Developer","Angular Developer"],
@@ -6,3 +6,42 @@ var typed = new Typed(".typing",{
     BackSpeed:60,
     loop:true
 })
+
+// ===Aside==== //
+
+// const nav = document.querySelector(".nav"),
+//     navList=nav.querySelectorAll(".li"),
+//     totalNavList = navList.length;
+//     for(let i=0;i<totalNavList;i++)
+//     {
+//         const a= navList[i].querySelector("a");
+//         a.addEventListener("click", function(){
+//             for(let j=0;totalNavList;j++){
+//                 navList[j].querySelector("a").classList.remove("active");
+//             }
+//             this.classList.add("active")
+            
+//         })
+//     }
+
+function myFunction(e) {
+    var elems = document.querySelectorAll(".active");
+    [].forEach.call(elems, function(el) {
+      el.classList.remove("active");
+    });
+    e.target.className = "active";
+  }
+
+  window.onbeforeunload = function() { 
+    window.setTimeout(function () { 
+        window.location = '/#home';
+    }, 0); 
+    window.onbeforeunload = null;
+    
+}
+
+
+
+  
+  
+
